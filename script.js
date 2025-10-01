a9b61a5834f7a9d443682ef74f2f4f5fb523a42a
killuaJ2002 <kjcodes2002@gmail.com>
2025-10-01 23:02:03 +0530

    added images to projects and modified content of the page

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const words = [
-  "Full-Stack Developer",
-  "ML Engineer",
-  "UI Designer",
-  "System Engineer",
-];
+const words = ["Full-Stack Developer", "UI Designer", "Low-level programmer"];
 let currentWordIndex = 0;
 let currentCharIndex = 0;
 let isDeleting = false;
